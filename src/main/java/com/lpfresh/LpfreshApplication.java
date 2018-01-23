8f9215b997fc7174173c441547c2770f5e9438ef
wd <wangding@handlecar.com>
2018-01-23 10:37:15 +0800

    start

---
+++ src/main/java/com/lpfresh/LpfreshApplication.java
@@ -0,0 +1,12 @@
+package com.lpfresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpfreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpfreshApplication.class, args);
+	}
+}
